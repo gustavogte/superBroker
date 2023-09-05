c0e6a60ef747c88a7ba30d852991bb5014edfd45
gustavogte <gustavogte@gmail.com>
2023-09-05 14:08:08 -0600

    modify user class

--- bank_pkg/user.py
+++ bank_pkg/user.py
@@ -6,16 +6,14 @@ class User:
         self.phone = phone
 
     def __str__(self):
-        return (
-            "Name: "
-            + self.name
-            + "\nPassword: "
-            + self.password
-            + "\nEmail: "
-            + self.email
-            + "\nPhone: "
-            + self.phone
+        return ("Name: " + self.name
+            + " Password: " + self.password
+            + " Email: " + self.email
+            + " Phone: " + self.phone
         )
+    
+    def __add__(self):
+        pass
 
     def sign_up():
         print("Sign up")
@@ -26,8 +24,23 @@ class User:
         user = User(name, password, email, phone)
         return user
 
+
     def login():
         print("Login")
         user = input("user: ")
         password = input("password: ")
         print(f"Hello User: {user}")
+
+def main():
+    print('hello gus')
+    user1 = User.sign_up()
+    user2 = User.sign_up()
+    users =[user1, user2]
+    print(user1)
+    print(type(user1))
+    print(user2)
+    print(type(user2))
+    print(users)
+    print(type(users))  
+
+main()
