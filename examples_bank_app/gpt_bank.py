796710a65f34438c263f207c8f0bdba2da3fcf88
gustavogte <gustavogte@gmail.com>
2023-09-09 01:11:00 -0600

    use bcrypt example

--- examples_bank_app/gpt_bank.py
+++ examples_bank_app/gpt_bank.py
@@ -1,7 +1,6 @@
 import csv
 import os
-import hashlib
-import secrets  # to generate a secure random salt
+import bcrypt
 
 class Bank:
     
@@ -11,33 +10,32 @@ class Bank:
     def initialize_csv(cls):
         if not os.path.exists(cls.file_path):
             with open(cls.file_path, "w", newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=["username", "password", "salt", "balance"])
+                writer = csv.DictWriter(file, fieldnames=["username", "password", "balance"])
                 writer.writeheader()
     
     @classmethod
-    def hash_password(cls, password, salt=None):
-        if not salt:
-            salt = secrets.token_hex(16)
-        password_hash = hashlib.sha256((password + salt).encode()).hexdigest()
-        return password_hash, salt
+    def hash_password(cls, password):
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed.decode('utf-8')
     
     @classmethod
     def sign_up(cls, username, password):
-        hashed_password, salt = cls.hash_password(password)
+        hashed_password = cls.hash_password(password)
         with open(cls.file_path, "a", newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=["username", "password", "salt", "balance"])
-            writer.writerow({"username": username, "password": hashed_password, "salt": salt, "balance": 0})  # initial balance is 0
+            writer = csv.DictWriter(file, fieldnames=["username", "password", "balance"])
+            writer.writerow({"username": username, "password": hashed_password, "balance": 0})  # initial balance is 0
     
     @classmethod
-    def check_password(cls, password, hashed_password, salt):
-        return hashed_password == cls.hash_password(password, salt)[0]
+    def check_password(cls, password, hashed_password):
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
     
     @classmethod
     def login(cls, username, password):
         with open(cls.file_path, "r") as file:
             reader = csv.DictReader(file)
             for row in reader:
-                if row["username"] == username and cls.check_password(password, row["password"], row["salt"]):
+                if row["username"] == username and cls.check_password(password, row["password"]):
                     return True, float(row["balance"])
             return False, None
 
